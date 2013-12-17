f112444e88b48f9d979910c7dd3111ffde651867
Wil Gieseler <wil@wilgieseler.com>
2013-12-16 17:59:40 -0800

    screw around with GCD

--- Classes/LUTReverser.m
+++ Classes/LUTReverser.m
@@ -10,12 +10,19 @@
 
 @interface LUTReverser ()
 @property (strong) KDTree *kdTree;
+@property (strong) NSArray *inputArray;
 @end
 
+float distancecalc(float x1, float y1, float z1, float x2, float y2, float z2) {
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float dz = z2 - z1;
+    return sqrt((float)(dx * dx + dy * dy + dz * dz));
+}
 
 @implementation LUTReverser
 
-- (void)buildSearchTreeWithSize:(NSUInteger)newSize {
+- (void)buildInputArray:(NSUInteger)newSize {
     NSLog(@"Building input array...");
     self.progressDescription = @"Building input array...";
     NSDate *startTime = [NSDate date];
@@ -48,31 +55,40 @@
         self.progress = (float)completedRs / (float)newSize * 0.33;
     });
     
+    self.inputArray = array;
+    
     NSLog(@"array built in: %f s", -[startTime timeIntervalSinceNow]);
+}
 
-    NSLog(@"Building search tree...");
-    self.progressDescription = @"Building search tree...";
-    NSDate *startTime2 = [NSDate date];
-
-    self.kdTree = [[KDTree alloc] initWithArray:array];
-    NSLog(@"Tree built in: %f s", -[startTime2 timeIntervalSinceNow]);
-    self.progress = 0.66;
+- (LUTColor *)colorNearestToR:(int)r g:(int)g b:(int)b {
+    float __block pickedDistance = FLT_MAX;
+    NSUInteger __block pickedIndex = 0;
+    [self.inputArray enumerateObjectsUsingBlock:^(NSArray *array, NSUInteger idx, BOOL *stop) {
+        float distance = distancecalc([array[0] floatValue], [array[1] floatValue], [array[2] floatValue], r, g, b);
+        if (distance < pickedDistance) {
+            pickedDistance = distance;
+            pickedIndex = idx;
+        }
+    }];
+    NSArray *subArray = self.inputArray[pickedIndex];
+    return subArray[3];
 }
 
 - (void)process {
+    
+    BOOL useTree = NO;
     
     NSUInteger outputSize = self.lut.lattice.size;
     
     NSOperationQueue* operationQueue = [[NSOperationQueue alloc] init];
 
     NSBlockOperation *buildOperation = [NSBlockOperation blockOperationWithBlock:^{
-        [self buildSearchTreeWithSize:outputSize * 3];
+        [self buildInputArray:outputSize * 3];
     }];
-    
 
     NSBlockOperation *findOperation = [NSBlockOperation blockOperationWithBlock:^{
         
-        NSLog(@"Building LUT from tree...");
+        NSLog(@"Building LUT...");
         self.progressDescription = @"Building LUT...";
         NSDate *startTime2 = [NSDate date];
         
@@ -80,17 +96,42 @@
         
         int maxValue = (int)outputSize - 1;
         
-        for (int r = 0; r < outputSize; r++) {
-            for (int g = 0; g < outputSize; g++) {
+        int __block completedOperations = 0;
+        NSUInteger totalOps = outputSize * outputSize * outputSize;
+        
+        NSLock *latticeLock = [[NSLock alloc] init];
+        
+        dispatch_apply(outputSize, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0) , ^(size_t rl){
+            int r = (int)rl;
+            dispatch_apply(outputSize, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0) , ^(size_t gl){
+                int g = (int)gl;
                 for (int b = 0; b < outputSize; b++) {
-                    KDLeaf *leaf = [self.kdTree findNearestNeighbor:@[@(remapint01(r, maxValue)),
-                                                                      @(remapint01(g, maxValue)),
-                                                                      @(remapint01(b, maxValue))]];
-                    [newLattice setColor:leaf.metadata r:r g:g b:b];
+                    if (useTree) {
+                        KDLeaf *leaf = [self.kdTree findNearestNeighbor:@[@(remapint01(r, maxValue)),
+                                                                          @(remapint01(g, maxValue)),
+                                                                          @(remapint01(b, maxValue))]];
+                        [latticeLock lock];
+                        [newLattice setColor:leaf.metadata r:r g:g b:b];
+                        [latticeLock unlock];
+                    }
+                    else {
+                        LUTColor *color = [self colorNearestToR:remapint01(r, maxValue) g:remapint01(g, maxValue) b:remapint01(b, maxValue)];
+                        [latticeLock lock];
+                        [newLattice setColor:color r:r g:g b:b];
+                        [latticeLock unlock];
+                    }
                 }
-            }
-            self.progress = (float)r / (float)outputSize * 0.33 + 0.66;
-        }
+                completedOperations += outputSize;
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (useTree) {
+                        self.progress = (float)completedOperations / (float)totalOps * 0.33 + 0.66;
+                    }
+                    else {
+                        self.progress = (float)completedOperations / (float)totalOps * 0.66 + 0.33;
+                    }
+                });
+            });
+        });
         
         NSLog(@"LUT built in: %f s", -[startTime2 timeIntervalSinceNow]);
         
@@ -99,9 +140,26 @@
     }];
     
     [findOperation addDependency:buildOperation];
-    
-    [operationQueue addOperation:buildOperation];
+
+    if (useTree) {
+        NSBlockOperation *buildTreeOperation = [NSBlockOperation blockOperationWithBlock:^{
+            [self buildInputArray:outputSize * 3];
+            
+            NSLog(@"Building search tree...");
+            self.progressDescription = @"Building search tree...";
+            NSDate *startTime2 = [NSDate date];
+            
+            self.kdTree = [[KDTree alloc] initWithArray:self.inputArray];
+            NSLog(@"Tree built in: %f s", -[startTime2 timeIntervalSinceNow]);
+            self.progress = 0.66;
+
+        }];
+        [findOperation addDependency:buildTreeOperation];
+        [operationQueue addOperation:buildTreeOperation];
+    }
+
     [operationQueue addOperation:findOperation];
+    [operationQueue addOperation:buildOperation];
 
 }
 
