87426f68dbcf723ac2f6400d230a774b66fbeff7
Greg Cotten <gregcotten@gmail.com>
2014-03-05 16:54:56 -0800

    OLUT formatter implementation

---
+++ Classes/LUTFormatterOLUT.m
@@ -0,0 +1,42 @@
+//
+//  LUTFormatterOLUT.m
+//  Pods
+//
+//  Created by Greg Cotten on 3/5/14.
+//
+//
+
+#import "LUTFormatterOLUT.h"
+
+@implementation LUTFormatterOLUT
+
++ (LUT *)LUTFromLines:(NSArray *)lines {
+    
+    NSMutableArray *redCurve = [NSMutableArray array];
+    NSMutableArray *greenCurve = [NSMutableArray array];
+    NSMutableArray *blueCurve = [NSMutableArray array];
+    
+
+    
+    NSMutableArray *trimmedLines = [NSMutableArray array];
+    
+    for (NSString *line in lines) {
+        NSString *trimmedLine = [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        if(trimmedLine.length > 0){
+            [trimmedLines addObject:trimmedLine];
+        }
+    }
+    
+    NSUInteger maxCodeValue = trimmedLines.count;
+    
+    for (NSString *line in trimmedLines) {
+        NSArray *splitLine = [line componentsSeparatedByString:@","];
+        [redCurve addObject:@(nsremapint01([splitLine[0] integerValue], maxCodeValue))];
+        [greenCurve addObject:@(nsremapint01([splitLine[1] integerValue], maxCodeValue))];
+        [blueCurve addObject:@(nsremapint01([splitLine[2] integerValue], maxCodeValue))];
+    }
+    
+    return [[LUT1D LUT1DWithRedCurve:redCurve greenCurve:greenCurve blueCurve:blueCurve] lutOfSize:33];
+}
+
+@end
