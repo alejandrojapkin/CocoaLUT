b7398c9ae6ba1fef8e2906940d86bc9e25f59047
Greg Cotten <gregcotten@gmail.com>
2014-03-29 00:25:47 -0700

    Discreet 1D write support

--- Classes/LUTFormatterDiscreet1DLUT.m
+++ Classes/LUTFormatterDiscreet1DLUT.m
@@ -19,7 +19,8 @@
     
     
     NSMutableArray *trimmedLines = [NSMutableArray array];
-    NSUInteger maxCodeValue = -1;
+    int maxCodeValue = -1;
+    int indices = -1;
     
     //trim for lut values only and grab the max code value
     for (NSString *line in lines) {
@@ -27,26 +28,58 @@
         if(trimmedLine.length > 0 && [trimmedLine rangeOfString:@"#"].location == NSNotFound && [trimmedLine rangeOfString:@"LUT"].location == NSNotFound){
             [trimmedLines addObject:trimmedLine];
         }
+        if([trimmedLine rangeOfString:@"Scale"].location != NSNotFound){
+            maxCodeValue = [[trimmedLine componentsSeparatedByString:@":"][1] intValue];
+        }
         if([trimmedLine rangeOfString:@"LUT"].location != NSNotFound){
-            maxCodeValue = [[trimmedLine componentsSeparatedByString:@" "][2] integerValue];
+            indices = [[trimmedLine componentsSeparatedByString:@" "][2] intValue];
         }
     }
     
     
     //get red values
-    for (int i = 0; i < maxCodeValue; i++) {
+    for (int i = 0; i < indices; i++) {
         [redCurve addObject:@(nsremapint01([trimmedLines[i] integerValue], maxCodeValue))];
     }
     //get green values
-    for (int i = (int)maxCodeValue; i < 2*maxCodeValue; i++) {
+    for (int i = indices; i < 2*indices; i++) {
         [greenCurve addObject:@(nsremapint01([trimmedLines[i] integerValue], maxCodeValue))];
     }
-    
-    for (int i = 2*(int)maxCodeValue; i < 3*maxCodeValue; i++) {
+    //get blue values
+    for (int i = 2*indices; i < 3*indices; i++) {
         [blueCurve addObject:@(nsremapint01([trimmedLines[i] integerValue], maxCodeValue))];
     }
     
     return [[LUT1D LUT1DWithRedCurve:redCurve greenCurve:greenCurve blueCurve:blueCurve] lutOfSize:33];
 }
+
++ (NSString *)stringFromLUT:(LUT *)lut {
+    
+    NSMutableString *string = [NSMutableString stringWithString:@""];
+    
+    [string appendString:[NSString stringWithFormat:@"#\n# Discreet LUT file\n#\tChannels: 3\n# Input Samples: 1024\n# Ouput Scale: 4095\n#\n# Exported from CocoaLUT\n#\n"]];
+    
+    LUT1D *lut1D = [[lut LUT1D] LUT1DByResizingToSize:1024];
+
+    
+    //write red
+    for (int i = 0; i < 1024; i++) {
+        [string appendString:[NSString stringWithFormat:@"%d\n", (int)([lut1D.redCurve[i] doubleValue]*4095.0) ]];
+    }
+    //write green
+    for (int i = 0; i < 1024; i++) {
+        [string appendString:[NSString stringWithFormat:@"%d\n", (int)([lut1D.greenCurve[i] doubleValue]*4095.0) ]];
+    }
+    //write blue
+    for (int i = 0; i < 1024; i++) {
+        [string appendString:[NSString stringWithFormat:@"%d\n", (int)([lut1D.blueCurve[i] doubleValue]*4095.0) ]];
+    }
+    
+    return string;
+    
+}
+
+
+
 
 @end
