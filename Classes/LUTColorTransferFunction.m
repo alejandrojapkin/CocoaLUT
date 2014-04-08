ecf5e48f473fbf8de36968e85783815de96d340d
Greg Cotten <gregcotten@gmail.com>
2014-04-07 18:54:35 -0700

    added AlexaLogC_V3 transfer function

--- Classes/LUTColorTransferFunction.m
+++ Classes/LUTColorTransferFunction.m
@@ -92,7 +92,8 @@
                            @"Gamma 1.8": [LUTColorTransferFunction LUTColorTransferFunctionWithGamma:1.8],
                            @"Linear": [LUTColorTransferFunction LUTColorTransferFunctionWithGamma:1.0],
                            @"Rec. 709": [LUTColorTransferFunction rec709TransferFunction],
-                           @"sRGB": [LUTColorTransferFunction sRGBTransferFunction]
+                           @"sRGB": [LUTColorTransferFunction sRGBTransferFunction],
+                           @"AlexaLogC_V3 EI800": [LUTColorTransferFunction arriLogCV3TransferFunctionWithEI:800]
                            };
     return dict;
 }
@@ -117,6 +118,53 @@
                                                                                             return (value <= .04045) ? value/12.92 : pow((value+.055)/1.055, 2.4);}
                                                                  linearToTransformedBlock1D:^double(double value){
                                                                                             return (value <= .0031308) ? 12.92*value : 1.055*pow(value, 1.0/2.4) - .055;}];
+}
+
++ (instancetype)arriLogCV3TransferFunctionWithEI:(double)EI{
+    //taken from ACES git repo
+    //https://github.com/ampas/aces-dev/blob/master/transforms/ctl/idt/vendorSupplied/arri/alexa/v3_IDT_maker.py
+    
+    double nominalEI = 400.0;
+    double blackSignal = 0.003907;
+    double midGraySignal = 0.01;
+    double encodingGain = 0.256598;
+    double encodingOffset = 0.391007;
+    
+    double cut = 1.0 / 9.0;
+    double slope = 1.0 / (cut * log(10));
+    double offset = log10(cut) - slope * cut;
+    double gain = EI / nominalEI;
+    double gray = midGraySignal / gain;
+    // The higher the EI, the lower the gamma
+    double encGain = (log(EI/nominalEI)/log(2) * (0.89 - 1) / 3 + 1) * encodingGain;
+    double encOffset = encodingOffset;
+    double nz;
+    for (int i = 0; i < 3; i++){
+        nz = ((95.0 / 1023.0 - encOffset) / encGain - offset) / slope;
+        encOffset = encodingOffset - log10(1 + nz) * encGain;
+    }
+    // Calculate some intermediate values
+    double a = 1.0 / gray;
+    double b = nz - blackSignal / gray;
+    double e = slope * a * encGain;
+    double f = encGain * (slope * b + offset) + encOffset;
+    // Manipulations so we can return relative exposure
+    double s = 4 / (0.18 * EI);
+    double t = blackSignal;
+    b = b + a * t;
+    a = a * s;
+    f = f + e * t;
+    e = e * s;
+    
+    cut = (cut-b) / a;
+    double c = encGain;
+    double d = encOffset;
+    
+    return [LUTColorTransferFunction LUTColorTransferFunctionWithTransformedToLinearBlock1D:^double(double value){
+                                                                                            return (value > e * cut + f) ? (pow(10, (value - d) / c) - b) / a: (value - f) / e;}
+        
+                                                                 linearToTransformedBlock1D:^double(double value){
+                                                                                            return (value > cut) ? c * log10(a * value + b) + d: e * value + f;}];
 }
 
 @end
