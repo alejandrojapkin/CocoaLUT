791d0a568e39a9418c8208c30723fbba6d8cf4d4
Greg Cotten <gregcotten@gmail.com>
2014-05-01 22:40:22 -0700

    LUT1DGraphView WIP

---
+++ Classes/LUT1DGraphView.h
@@ -0,0 +1,13 @@
+//
+//  LUT1DGraphView.h
+//  Pods
+//
+//  Created by Greg Cotten on 5/1/14.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LUT1DGraphView : NSView
+
+@end
---
+++ Classes/LUT1DGraphView.m
@@ -0,0 +1,29 @@
+//
+//  LUT1DGraphView.m
+//  Pods
+//
+//  Created by Greg Cotten on 5/1/14.
+//
+//
+
+#import "LUT1DGraphView.h"
+
+@implementation LUT1DGraphView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
