d1d50803feb5b56e272b416267f0b76d97e195d6
gregcotten <gregcotten@gmail.com>
2014-06-18 15:28:29 -0500

    preview 3d scene will show result color on dots (not identity)

--- Classes/LUTPreviewScene.m
+++ Classes/LUTPreviewScene.m
@@ -129,7 +129,7 @@
         SCNBox *dot = [SCNBox boxWithWidth:2.0*radius height:2.0*radius length:2.0*radius chamferRadius:radius];
         dot.chamferSegmentCount = 1.0; //efficient for rendering but makes it look like a polygon
         
-        dot.firstMaterial.diffuse.contents = [identityColor remappedFromInputLow:lut3D.inputLowerBound inputHigh:lut3D.inputUpperBound outputLow:0 outputHigh:1 bounded:NO].systemColor;
+        dot.firstMaterial.diffuse.contents = [transformedColor remappedFromInputLow:lut3D.inputLowerBound inputHigh:lut3D.inputUpperBound outputLow:0 outputHigh:1 bounded:NO].systemColor;
         
         LUTColorNode *node = (LUTColorNode*)[LUTColorNode nodeWithGeometry:dot];
         node.identityColor = identityColor;
