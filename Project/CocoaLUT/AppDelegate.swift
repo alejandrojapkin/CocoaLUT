e5cfcad80b81e7bfb7a9d4ce6e0c8810c74b544e
Wil Gieseler <wil@wilgieseler.com>
2014-06-25 22:07:58 -0700

    Revert 0eb35dc..40e5948
    This rolls back to commit 0eb35dc1b5292ec0da205aa63c0b10ee01071f97.

---
+++ Project/CocoaLUT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaLUT
+//
+//  Created by Wil Gieseler on 6/25/14.
+//  Copyright (c) 2014 Wil Gieseler. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
