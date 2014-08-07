ba7d9b0ca3599a94a355cd8674dd9665260913c7
gregcotten <gregcotten@gmail.com>
2014-08-07 14:20:31 -0700

    Resolve DAT r/w support

---
+++ Classes/LUTFormatterResolveDAT.h
@@ -0,0 +1,13 @@
+//
+//  LUTFormatterResolveDAT.h
+//  Pods
+//
+//  Created by Greg Cotten on 8/6/14.
+//
+//
+
+#import "LUTFormatter.h"
+
+@interface LUTFormatterResolveDAT : LUTFormatter
+
+@end
---
+++ Classes/LUTFormatterResolveDAT.m
@@ -0,0 +1,158 @@
+//
+//  LUTFormatterResolveDAT.m
+//  Pods
+//
+//  Created by Greg Cotten on 8/6/14.
+//
+//
+
+#import "LUTFormatterResolveDAT.h"
+
+@implementation LUTFormatterResolveDAT
+
++ (void)load{
+    [super load];
+}
+
++ (LUT *)LUTFromLines:(NSArray *)lines{
+    NSUInteger cubeLinesStartIndex = findFirstLUTLineInLines(lines, @" ", 3, 0);
+
+    if(cubeLinesStartIndex == -1){
+        @throw [NSException exceptionWithName:@"LUTParserError" reason:@"Couldn't find start of LUT data lines." userInfo:nil];
+    }
+
+    NSArray *headerLines = [lines subarrayWithRange:NSMakeRange(0, cubeLinesStartIndex)];
+    NSUInteger cubeSize = -1;
+    for(NSString *untrimmedLine in headerLines){
+        NSString *line = [untrimmedLine stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        if([line rangeOfString:@"#"].location == NSNotFound){
+            if ([line rangeOfString:@"3DLUTSIZE"].location != NSNotFound) {
+                NSArray *splitLine = [line componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                if(splitLine.count == 2 && stringIsValidNumber(splitLine[1])){
+                    cubeSize = [splitLine[1] integerValue];
+
+                }
+                else{
+                    @throw [NSException exceptionWithName:@"CubeLUTParseError"
+                                                   reason:@"Size parameter is invalid."
+                                                 userInfo:nil];
+                }
+            }
+        }
+    }
+
+    LUT3D *lut = [LUT3D LUTOfSize:cubeSize inputLowerBound:0 inputUpperBound:1];
+
+    NSUInteger currentCubeIndex = 0;
+    for (NSString *line in [lines subarrayWithRange:NSMakeRange(cubeLinesStartIndex, lines.count - cubeLinesStartIndex)]) {
+
+        if (line.length > 0 && [line rangeOfString:@"#"].location == NSNotFound) {
+            NSArray *splitLine = [line componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            splitLine = arrayWithEmptyElementsRemoved(splitLine);
+            if (splitLine.count == 3) {
+                for(NSString *checkLine in splitLine){
+                    if(stringIsValidNumber(checkLine) == NO){
+                        @throw [NSException exceptionWithName:@"CubeLUTParseError" reason:[NSString stringWithFormat:@"NaN detected at line %i", (int)currentCubeIndex+(int)cubeLinesStartIndex] userInfo:nil];
+                    }
+                }
+
+                // Valid cube line
+                LUTColorValue blueValue = ((NSString *)splitLine[0]).doubleValue;
+                LUTColorValue greenValue = ((NSString *)splitLine[1]).doubleValue;
+                LUTColorValue redValue = ((NSString *)splitLine[2]).doubleValue;
+
+                LUTColor *color = [LUTColor colorWithRed:redValue green:greenValue blue:blueValue];
+
+                NSUInteger redIndex = currentCubeIndex % cubeSize;
+                NSUInteger greenIndex = ( (currentCubeIndex % (cubeSize * cubeSize)) / (cubeSize) );
+                NSUInteger blueIndex = currentCubeIndex / (cubeSize * cubeSize);
+
+                [lut setColor:color r:redIndex g:greenIndex b:blueIndex];
+
+                currentCubeIndex++;
+            }
+        }
+    }
+    if(currentCubeIndex < cubeSize*cubeSize*cubeSize){
+        @throw [NSException exceptionWithName:@"CubeLUTParseError" reason:@"Incomplete data lines" userInfo:nil];
+    }
+
+
+    lut.passthroughFileOptions = @{[self formatterID]:@{@"fileTypeVariant": @"Resolve"}};
+
+    return lut;
+}
+
++ (NSString *)stringFromLUT:(LUT *)lut withOptions:(NSDictionary *)options{
+    NSMutableString *string = [[NSMutableString alloc] init];
+
+
+
+    NSUInteger lutSize = lut.size;
+    NSUInteger arrayLength = lutSize * lutSize * lutSize;
+
+    [string appendString:[NSString stringWithFormat:@"3DLUTSIZE %i\n\n", (int)lutSize]];
+
+    for (int i = 0; i < arrayLength; i++) {
+        int blueIndex = i % lutSize;
+        int greenIndex = ((i % (lutSize * lutSize)) / (lutSize) );
+        int redIndex= i / (lutSize * lutSize);
+
+        LUTColor *color = [lut colorAtR:redIndex g:greenIndex b:blueIndex];
+
+        [string appendString:[NSString stringWithFormat:@"%.6f %.6f %.6f", color.red, color.green, color.blue]];
+
+
+        if(i != arrayLength - 1) {
+            [string appendString:@"\n"];
+        }
+
+    }
+
+    return string;
+}
+
+
++ (LUTFormatterOutputType)outputType{
+    return LUTFormatterOutputType3D;
+}
+
++ (NSString *)formatterName{
+    return @"Resolve DAT 3D LUT";
+}
+
++ (NSString *)formatterID{
+    return @"resolveDAT";
+}
+
++ (BOOL)canRead{
+    return YES;
+}
+
++ (BOOL)canWrite{
+    return YES;
+}
+
++ (NSString *)utiString{
+    return @"com.blackmagicdesign.dat";
+}
+
++ (NSArray *)fileExtensions{
+    return @[@"dat"];
+}
+
++ (NSArray *)allOptions{
+
+    NSDictionary *options = @{@"fileTypeVariant":@"Resolve"};
+
+    return @[options];
+
+}
+
++ (NSDictionary *)defaultOptions{
+    NSDictionary *dictionary = @{@"fileTypeVariant":@"Resolve"};
+    
+    return @{[self formatterID]: dictionary};
+}
+
+@end
