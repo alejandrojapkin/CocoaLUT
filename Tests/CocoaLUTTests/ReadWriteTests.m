55cb2763d27e2c14b16a361c3b65f3e39d395293
Greg Cotten <gregcotten@gmail.com>
2014-08-11 14:32:28 -0700

    ResolveDAT and FSIDAT r/w tests

--- Tests/CocoaLUTTests/ReadWriteTests.m
+++ Tests/CocoaLUTTests/ReadWriteTests.m
@@ -16,6 +16,8 @@
 #import <CocoaLUT/LUTFormatterOLUT.h>
 #import <CocoaLUT/LUTFormatterILUT.h>
 #import <CocoaLUT/LUTFormatterCMSTestPattern.h>
+#import <CocoaLUT/LUTFormatterFSIDAT.h>
+#import <CocoaLUT/LUTFormatterResolveDAT.h>
 
 
 @interface ReadWriteTests : XCTestCase
@@ -113,6 +115,58 @@
     LUT3D *identityLUT = [LUT3D LUTIdentityOfSize:17 inputLowerBound:0 inputUpperBound:1];
 
     LUTFormatter *formatter = [LUTFormatter LUTFormatterWithID:[LUTFormatterClipster formatterID]];
+
+    NSURL *lutURL = [self.class uniqueTempFileURLWithFileExtension:[[formatter class] fileExtensions][0]];
+
+    BOOL writeSuccess = [identityLUT writeToURL:lutURL
+                                     atomically:YES
+                                    formatterID:[[formatter class] formatterID]
+                                        options:nil
+                                     conformLUT:YES];
+
+    XCTAssert(writeSuccess && [lutURL checkResourceIsReachableAndReturnError:nil], @"LUT didn't write successfully.");
+
+    XCTAssertEqual([LUTFormatter LUTFormatterValidForReadingURL:lutURL], [formatter class], @"LUT isn't recognized with the correct formatter.");
+
+    LUT *readLUT = [LUT LUTFromURL:lutURL];
+
+    [[NSFileManager defaultManager] removeItemAtURL:lutURL error:nil];
+
+    XCTAssert([readLUT equalsLUT:identityLUT]);
+    
+    
+}
+
+- (void)testReadWriteResolveDAT {
+    LUT3D *identityLUT = [LUT3D LUTIdentityOfSize:33 inputLowerBound:0 inputUpperBound:1];
+
+    LUTFormatter *formatter = [LUTFormatter LUTFormatterWithID:[LUTFormatterResolveDAT formatterID]];
+
+    NSURL *lutURL = [self.class uniqueTempFileURLWithFileExtension:[[formatter class] fileExtensions][0]];
+
+    BOOL writeSuccess = [identityLUT writeToURL:lutURL
+                                     atomically:YES
+                                    formatterID:[[formatter class] formatterID]
+                                        options:nil
+                                     conformLUT:YES];
+
+    XCTAssert(writeSuccess && [lutURL checkResourceIsReachableAndReturnError:nil], @"LUT didn't write successfully.");
+
+    XCTAssertEqual([LUTFormatter LUTFormatterValidForReadingURL:lutURL], [formatter class], @"LUT isn't recognized with the correct formatter.");
+
+    LUT *readLUT = [LUT LUTFromURL:lutURL];
+
+    [[NSFileManager defaultManager] removeItemAtURL:lutURL error:nil];
+
+    XCTAssert([readLUT equalsLUT:identityLUT]);
+    
+    
+}
+
+- (void)testReadWriteFSIDAT {
+    LUT3D *identityLUT = [LUT3D LUTIdentityOfSize:64 inputLowerBound:0 inputUpperBound:1];
+
+    LUTFormatter *formatter = [LUTFormatter LUTFormatterWithID:[LUTFormatterFSIDAT formatterID]];
 
     NSURL *lutURL = [self.class uniqueTempFileURLWithFileExtension:[[formatter class] fileExtensions][0]];
 
