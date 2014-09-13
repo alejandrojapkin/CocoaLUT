a58eed7d82307d761f0c38904f7e43bc7961a123
gregcotten <gregcotten@gmail.com>
2014-09-12 18:16:05 -0700

    somehow forgot test for r/w unwrapped cube

--- Tests/CocoaLUTTests/ReadWriteTests.m
+++ Tests/CocoaLUTTests/ReadWriteTests.m
@@ -16,6 +16,7 @@
 #import <CocoaLUT/LUTFormatterOLUT.h>
 #import <CocoaLUT/LUTFormatterILUT.h>
 #import <CocoaLUT/LUTFormatterCMSTestPattern.h>
+#import <CocoaLUT/LUTFormatterUnwrappedTexture.h>
 #import <CocoaLUT/LUTFormatterFSIDAT.h>
 #import <CocoaLUT/LUTFormatterResolveDAT.h>
 
@@ -245,6 +246,32 @@
     LUT3D *identityLUT = [LUT3D LUTIdentityOfSize:33 inputLowerBound:0 inputUpperBound:1];
 
     LUTFormatter *formatter = [LUTFormatter LUTFormatterWithID:[LUTFormatterCMSTestPattern formatterID]];
+
+    NSURL *lutURL = [self.class uniqueTempFileURLWithFileExtension:[[formatter class] fileExtensions][0]];
+
+    BOOL writeSuccess = [identityLUT writeToURL:lutURL
+                                     atomically:YES
+                                    formatterID:[[formatter class] formatterID]
+                                        options:nil
+                                     conformLUT:YES];
+
+    XCTAssert(writeSuccess && [lutURL checkResourceIsReachableAndReturnError:nil], @"LUT didn't write successfully.");
+
+    XCTAssertEqual([LUTFormatter LUTFormatterValidForReadingURL:lutURL], [formatter class], @"LUT isn't recognized with the correct formatter.");
+
+    LUT *readLUT = [LUT LUTFromURL:lutURL];
+
+    [[NSFileManager defaultManager] removeItemAtURL:lutURL error:nil];
+
+    XCTAssert([readLUT equalsLUT:identityLUT]);
+    
+    
+}
+
+- (void)testReadWriteUnwrappedCube {
+    LUT3D *identityLUT = [LUT3D LUTIdentityOfSize:33 inputLowerBound:0 inputUpperBound:1];
+
+    LUTFormatter *formatter = [LUTFormatter LUTFormatterWithID:[LUTFormatterUnwrappedTexture formatterID]];
 
     NSURL *lutURL = [self.class uniqueTempFileURLWithFileExtension:[[formatter class] fileExtensions][0]];
 
